972bd8e5ca5354197aa6328b9eb09887a1ac2423
Florian Ratz <florian.ratz@supsign.ch>
2025-03-17 14:12:03 +0100

    init

---
+++ src/NinjaRMM.php
@@ -0,0 +1,6 @@
+<?php
+
+namespace Supsign\Laravel;
+
+class NinjaRMM
+{}
---
+++ src/NinjaRMMServiceProvider.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Supsign\Laravel;
+
+use Illuminate\Support\ServiceProvider;
+
+class NinjaRMMServiceProvider extends ServiceProvider
+{
+	public function boot() {}
+	public function register() {}
+}
